e4966cf7ee88ee1e7e2f86cd6074a77bf90cc28c
Xpki <hackerboi8900@gmail.com>
2024-05-01 19:17:39 -0500

    ww

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/tiw/',
-    bare:'/static/bare/',
+    bare:'/uv/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
