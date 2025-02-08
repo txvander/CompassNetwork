a034413880fb9645d85654e1b2ed9e32de4b2068
NEXTGENGAMING <xxphoeni@icloud.com>
2025-02-08 16:32:09 +0000

    added a proxy

---
+++ src/js/fs.js
@@ -0,0 +1,12 @@
+function toggleFullscreen() {
+    const iframe = document.getElementById('proxyFrame');
+    if (iframe.requestFullscreen) {
+        iframe.requestFullscreen();
+    } else if (iframe.mozRequestFullScreen) { 
+        iframe.mozRequestFullScreen();
+    } else if (iframe.webkitRequestFullscreen) { 
+        iframe.webkitRequestFullscreen();
+    } else if (iframe.msRequestFullscreen) { 
+        iframe.msRequestFullscreen();
+    }
+}
