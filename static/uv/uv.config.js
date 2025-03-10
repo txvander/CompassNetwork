2d53071e01cb9be09a9a1b45e89f3940b7871931
NEXTGENGAMING <xxphoeni@icloud.com>
2025-03-10 02:40:26 +0000

    new font for the search bar

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/uv/service/',
-    bare: "https://benrogo.us.kg/",
+    bare: "https://cdn.starttiw.org/",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
