3cd0900e63905d781d90349c4cadd59372e9c81f
NEXTGENGAMING <xxphoeni@icloud.com>
2025-04-08 05:33:59 +0000

    settings update

--- tabcloak.js
+++ tabcloak.js
@@ -1,7 +1,39 @@
-const cloakBtn = document.getElementById('cloakBtn');
+const faviconMap = {
+    googleCloak: {
+        title: "Google",
+        icon: "https://www.google.com/favicon.ico" 
+    },
+    youtubeCloak: {
+        title: "YouTube",
+        icon: "images/Tabcloaks/yt2.png" 
+    },
+    desmosCloak: {
+        title: "Desmos",
+        icon: "images/Tabcloaks/desmos.png" 
+    },
+    newtabCloak:{
+        title: "Newtab",
+        icon:"images/Tabcloaks/newtab.png"
+    },
+};
 
-cloakBtn.addEventListener('click', () => {
-    document.title = "Google";
-    document.getElementById('cloak').href = 
-        `https://www.google.com/favicon.ico?${Date.now()}`;
+function setFavicon(url) {
+    let link = document.querySelector("link[rel~='icon']");
+    if (!link) {
+        link = document.createElement("link");
+        link.rel = "icon";
+        document.head.appendChild(link);
+    }
+    link.href = `${url}?t=${Date.now()}`;  
+}
+
+Object.keys(faviconMap).forEach(id => {
+    const btn = document.getElementById(id);
+    if (btn) {
+        btn.addEventListener('click', () => {
+            const { title, icon } = faviconMap[id];
+            document.title = title;
+            setFavicon(icon);
+        });
+    }
 });
