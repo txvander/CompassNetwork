d0ce39dfb75c2f7d6929a6569dde4c629d36ac00
NEXTGENGAMING <xxphoeni@icloud.com>
2025-05-16 06:39:24 +0000

    switched the bare server back

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/uv/service/',
-    bare: "http://localhost:8080",
+    bare: "https://cdn.starttiw.org",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
