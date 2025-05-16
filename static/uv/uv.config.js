3e45a4bc664329c7842a9d40c6ec05ceb139d398
NEXTGENGAMING <xxphoeni@icloud.com>
2025-05-16 06:01:50 +0000

    added another way of hosting

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/uv/service/',
-    bare: "https://cdn.starttiw.org/",
+    bare: "http://localhost:8080",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
